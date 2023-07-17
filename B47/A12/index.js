1d46da0e54d799ac117922f9610d136b14244f00
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:44:28 +0500

    A12

---
+++ B47/A12/index.js
@@ -0,0 +1,4 @@
+var firendsName = ['Umer', 'Khawar', 'Tamoor', 'Bilal'];
+firendsName.forEach(function (friend) {
+    console.log("Hello my dear friend ".concat(friend, ". Hope you are doing well."));
+});
---
+++ B47/A12/index.ts
@@ -0,0 +1,4 @@
+var firendsName=['Umer','Khawar','Tamoor','Bilal'];
+firendsName.forEach(friend => {
+    console.log(`Hello my dear friend ${friend}. Hope you are doing well.`);
+});
