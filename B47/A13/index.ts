f3dddbdca36a0e2c627399284075776343d68d3d
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:48:49 +0500

    A13

---
+++ B47/A13/index.ts
@@ -0,0 +1,8 @@
+var ownTransportationArray=[];
+ownTransportationArray.push(`I would like to have toyta fortuner.`);
+ownTransportationArray.push(`Fortuner have different variants.`);
+ownTransportationArray.push(`I would loike to have top varian i.e. Sigma.`);
+ownTransportationArray.push(`Fortuner is 2700 cc SUV.`);
+ownTransportationArray.forEach(element => {
+    console.log(element);
+});
