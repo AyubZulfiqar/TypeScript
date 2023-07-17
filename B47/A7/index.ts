2bf0474999b23daef10296a5182a38aa013806bd
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:38:32 +0500

    A9 Comment Added

--- B47/A7/index.ts
+++ B47/A7/index.ts
@@ -1,19 +1,26 @@
+//------------Addition----------
 var num1 = 3;
 var num2 = 5;
 var addition = num1 + num2;
 console.log(`Addition: ${addition}`);
+//------------------------------
 
+//------------Subtraction-------
 num1 = 10;
 num2 = 2;
 var subtraction = num1 - num2;
 console.log(`Subtraction: ${subtraction}`);
+//------------------------------
 
+//----------Multiplication------
 num1 = 4;
 num2 = 2;
 var multiplication = num1 * num2;
 console.log(`Multiplication: ${multiplication}`);
-
+//------------------------------
+//------------Division----------
 num1 = 16;
 num2 = 2;
 var dividion = num1 / num2;
 console.log(`Division: ${dividion}`);
+//------------------------------
