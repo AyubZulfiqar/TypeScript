b8b06774eb856cef043dd429c52420f79cecea12
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:33:02 +0500

    A7

---
+++ B47/A7/index.js
@@ -0,0 +1,16 @@
+var num1 = 3;
+var num2 = 5;
+var addition = num1 + num2;
+console.log("Addition: ".concat(addition));
+num1 = 10;
+num2 = 2;
+var subtraction = num1 - num2;
+console.log("Subtraction: ".concat(subtraction));
+num1 = 4;
+num2 = 2;
+var multiplication = num1 * num2;
+console.log("Multiplication: ".concat(multiplication));
+num1 = 16;
+num2 = 2;
+var dividion = num1 / num2;
+console.log("Division: ".concat(dividion));
---
+++ B47/A7/index.ts
@@ -0,0 +1,19 @@
+var num1 = 3;
+var num2 = 5;
+var addition = num1 + num2;
+console.log(`Addition: ${addition}`);
+
+num1 = 10;
+num2 = 2;
+var subtraction = num1 - num2;
+console.log(`Subtraction: ${subtraction}`);
+
+num1 = 4;
+num2 = 2;
+var multiplication = num1 * num2;
+console.log(`Multiplication: ${multiplication}`);
+
+num1 = 16;
+num2 = 2;
+var dividion = num1 / num2;
+console.log(`Division: ${dividion}`);
