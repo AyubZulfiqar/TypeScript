a7d01db2b7c851ccc19e2772fba273a985d91396
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:52:41 +0500

    A14

---
+++ B47/A14/index.js
@@ -0,0 +1,4 @@
+var invitation = ['Nana', 'Nani', 'Dada', 'Dadi'];
+invitation.forEach(function (element) {
+    console.log("Hi ".concat(element, ", I would like to invite you dinner on birthday of your grandson."));
+});
---
+++ B47/A14/index.ts
@@ -0,0 +1,4 @@
+var invitation=['Nana','Nani','Dada','Dadi'];
+invitation.forEach(element => {
+    console.log(`Hi ${element}, I would like to invite you dinner on birthday of your grandson.`);
+});
