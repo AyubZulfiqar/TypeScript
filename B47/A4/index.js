7110a3b25d8c5b473eb8a930bae0a8837ad1457b
ayub-ec <ayub@eventcombo.com>
2023-07-17 23:22:12 +0500

    A5

--- B47/A4/index.js
+++ B47/A4/index.js
@@ -1,3 +1,1 @@
-var authorName = 'Albert Einstein';
-var quote = 'A person who never made a mistake never tried anything new.';
-console.log("".concat(authorName, " once said, \u201C").concat(quote, "\""));
+console.log("Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
--- B47/A4/index.ts
+++ B47/A4/index.ts
@@ -1,3 +1,1 @@
-var authorName = 'Albert Einstein';
-var quote = 'A person who never made a mistake never tried anything new.';
-console.log(`${authorName} once said, “${quote}"`);
+console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.”`);
